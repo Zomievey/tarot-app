b7817673170ce4db054d4dd9e52fc834fb0b2149
Zomievey <haylee_hunter@aol.com>
2024-10-28 23:56:53 -0500

    Ignore font files from @expo/vector-icons in Webpack and use dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
+"use client";
+
 import React from "react";
-import {
-  View,
-  Text,
-  ImageBackground,
-  Platform,
-  ScrollView,
-} from "react-native";
+import { View, Text, ImageBackground, Platform, ScrollView } from "react-native";
 import styles from "./globalStyles";
 import { Asset } from "expo-asset";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const FontAwesome = dynamic(() =>
-  import('@expo/vector-icons').then((mod) => mod.FontAwesome), { ssr: false }
+const FontAwesome = dynamic(
+  () => import("@expo/vector-icons").then((mod) => mod.FontAwesome),
+  { ssr: false }
 );
-
 
 export default function Index() {
   const backgroundImage =
     Platform.OS === "web"
-      ? "/images/main-background.png" // Path for web, accessible via public folder
+      ? "/images/main-background.png"
       : Asset.fromModule(require("../assets/images/main-background.png")).uri;
 
   const LinkComponent =
@@ -32,36 +28,29 @@ export default function Index() {
     <ImageBackground source={{ uri: backgroundImage }} style={styles.container}>
       <View style={styles.overlayContainer}>
         <View style={styles.overlay}>
-          <Text style={[styles.title, { fontFamily: titleFont }]}>
-            Pocket Tarot
-          </Text>
+          <Text style={[styles.title, { fontFamily: titleFont }]}>Pocket Tarot</Text>
 
-          {/* Replace Links */}
-          <LinkComponent href='/singleCard/'>
+          <LinkComponent href="/singleCard/">
             <Text style={styles.linkText}>
-              <FontAwesome name='star-o' size={20} color='white' /> CARD OF THE
-              DAY
+              <FontAwesome name="star-o" size={20} color="white" /> CARD OF THE DAY
             </Text>
           </LinkComponent>
 
-          <LinkComponent href='/threeCard/'>
+          <LinkComponent href="/threeCard/">
             <Text style={styles.linkText}>
-              <FontAwesome name='star-o' size={20} color='white' /> THREE CARD
-              SPREAD
+              <FontAwesome name="star-o" size={20} color="white" /> THREE CARD SPREAD
             </Text>
           </LinkComponent>
 
-          <LinkComponent href='/fiveCard/'>
+          <LinkComponent href="/fiveCard/">
             <Text style={styles.linkText}>
-              <FontAwesome name='star-o' size={20} color='white' /> FIVE CARD
-              SPREAD
+              <FontAwesome name="star-o" size={20} color="white" /> FIVE CARD SPREAD
             </Text>
           </LinkComponent>
 
-          <LinkComponent href='/customizeDeck/'>
+          <LinkComponent href="/customizeDeck/">
             <Text style={styles.linkText}>
-              <FontAwesome name='star-o' size={20} color='white' /> CUSTOMIZE
-              DECK
+              <FontAwesome name="star-o" size={20} color="white" /> CUSTOMIZE DECK
             </Text>
           </LinkComponent>
         </View>
